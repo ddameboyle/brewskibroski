47579098693b5ab668715118c93687d19833569b
MKS-students <mks.students@hackreactor.com>
2016-01-10 19:45:10 -0800

    testing

---
+++ client/controllers/auth.js
@@ -0,0 +1,28 @@
+angular.module('main.auth', [])
+
+.controller('AuthController', function ($scope, $window, $location, Auth) {
+  $scope.user = {};
+
+  $scope.signin = function () {
+    Auth.signin($scope.user)
+      .then(function (token) {
+        $window.localStorage.setItem('com.shortly', token);
+        $location.path('/main');
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  $scope.signup = function () {
+    Auth.signup($scope.user)
+      .then(function (token) {
+        $window.localStorage.setItem('com.shortly', token);
+        $location.path('/main');
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+});
+
